11b289b79bc1a704a54edd8853f6994830670f65
jam09 <098765trejo@gmail.com>
2021-03-25 06:27:10 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+// back tot top
+
+let backToTopBtn = document.querySelector('.back-to-top')
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = 'flex'
+    } else {
+        backToTopBtn.style.display = 'none'
+    }
+}
+
+// top nav menu
+
+let menuItems = document.getElementsByClassName('menu-item')
+
+Array.from(menuItems).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currMenu = document.querySelector('.menu-item.active')
+        currMenu.classList.remove('active')
+        item.classList.add('active')
+    }
+})
+
+// food category
+
+let foodMenuList = document.querySelector('.food-item-wrap')
+
+let foodCategory = document.querySelector('.food-category')
+
+let categories = foodCategory.querySelectorAll('button')
+
+Array.from(categories).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currCat = foodCategory.querySelector('button.active')
+        currCat.classList.remove('active')
+        e.target.classList.add('active')
+        foodMenuList.classList = 'food-item-wrap ' + e.target.getAttribute('data-food-type')
+    }
+})
+
+// on scroll animation
+
+let scroll = window.requestAnimationFrame || function(callback) { window.setTimeout(callback, 1000 / 60) }
+
+let elToShow = document.querySelectorAll('.play-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    return (
+        (rect.top <= 0 && rect.bottom >= 0) ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    )
+}
+
+loop = () => {
+    elToShow.forEach((item, index) => {
+        if (isElInViewPort(item)) {
+            item.classList.add('start')
+        } else {
+            item.classList.remove('start')
+        }
+    })
+
+    scroll(loop)
+}
+
+loop()
+
+// mobile nav
+
+let bottomNavItems = document.querySelectorAll('.mb-nav-item')
+
+let bottomMove = document.querySelector('.mb-move-item')
+
+bottomNavItems.forEach((item, index) => {
+    item.onclick = (e) => {
+        console.log('object')
+        let crrItem = document.querySelector('.mb-nav-item.active')
+        crrItem.classList.remove('active')
+        item.classList.add('active')
+        bottomMove.style.left = index * 25 + '%'
+    }
+})
+
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
